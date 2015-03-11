88f60b5e47d12b93f15ac95ee2e91c1baa150f92
Rocky <relewis4@uncg.edu>
2015-03-11 13:10:28 -0400

    Changed main activity to games play activity

--- app/src/main/java/com/rockywebdeveloper/marblemadness/GamesPlayActivity.java
+++ app/src/main/java/com/rockywebdeveloper/marblemadness/GamesPlayActivity.java
@@ -6,12 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class GamesPlayActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_games_play);
     }
 
 
